3f5e33da0430697261253d0c777a299bfba77b28
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2021-12-05 16:37:45 -0600

    add model wrapper and speech recognizer

---
+++ ScanningApp/Controllers/ModelWrapper.swift
@@ -0,0 +1,129 @@
+//
+//  ModelWrapper.swift
+//  ScanningApp
+//
+//  Created by Amor on 2021/12/4.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import Foundation
+
+// I set this protocol because I want to pass values to the viewController
+protocol ModelWrapperDelegate: AnyObject{
+    // if there will be a change in the prediction, then this method is called
+    func predictionWillChange(value:String)
+}
+
+class ModelWrapper: NSObject{
+    
+    weak var delegate: ModelWrapperDelegate?
+    
+    // Model Server address
+    private var serverURL = ""
+    // this key is for authentication, it will be stored in the cookie
+    private let passKey = "h7hdg43scbmdTRY7hbv321szxcdOPd2mz1"
+    // use chatId to identify if it's the same conversation
+    private var chatId = ""
+    
+    private var session: URLSession = {
+        let sessionConfig = URLSessionConfiguration.ephemeral
+        
+        sessionConfig.timeoutIntervalForRequest = 20.0
+        sessionConfig.timeoutIntervalForResource = 10.0
+        sessionConfig.httpMaximumConnectionsPerHost = 1
+        
+        return URLSession(configuration: sessionConfig)
+    }()
+    
+    // get the prediction result
+    private var prediction: String = "" {
+        willSet(newValue){
+            // It's to delegate to viewController so that it can receive the event of value changed in order to update UI
+            delegate?.predictionWillChange(value: newValue)
+        }
+    }
+    
+    init(url:String, delegate:ModelWrapperDelegate) {
+        self.serverURL = url
+        self.delegate = delegate
+    }
+    
+    // reset the chatId so we can have thoroughly new conversation
+    func resetChatId() {
+        chatId = ""
+    }
+    
+    func getPrediction(_ recordingContent:String, chatId:String){
+        let baseURL = "\(serverURL)/GetNewAnswer"
+        let postURL = URL(string: "\(baseURL)")
+        
+        // create a custom HTTP POST request
+        var request = URLRequest(url: postURL!)
+        
+        // data to send in body of post request (send arguments as json)
+        let jsonUpload:NSDictionary = ["newInput":recordingContent, "chatId":chatId]
+        
+        
+        let requestBody:Data? = self.convertDictionaryToData(with:jsonUpload)
+        
+        request.httpMethod = "POST"
+        request.httpBody = requestBody
+        
+        // set cookies for authentication
+        request.setValue("pass=\(passKey)", forHTTPHeaderField: "Cookie")
+        request.httpShouldHandleCookies = true
+        
+        let postTask : URLSessionDataTask = self.session.dataTask(with: request,
+                                                                  completionHandler:{
+                        (data, response, error) in
+                        if(error != nil){
+                            if let res = response{
+                                print("Response:\n",res)
+                            }
+                        }
+                        else{ // no error we are aware of
+                            
+                            let jsonDictionary = self.convertDataToDictionary(with: data)
+                            print("jsonDic: \(jsonDictionary)")
+                            let response:String = jsonDictionary["response"]! as! String
+                            self.prediction = response
+                            self.chatId = jsonDictionary["chatId"]! as! String
+                        }
+                                                                    
+        })
+        
+        postTask.resume() // start the task
+    }
+    
+   
+    //MARK: JSON Conversion Functions
+    func convertDictionaryToData(with jsonUpload:NSDictionary) -> Data?{
+        do { // try to make JSON and deal with errors using do/catch block
+            let requestBody = try JSONSerialization.data(withJSONObject: jsonUpload, options:JSONSerialization.WritingOptions.prettyPrinted)
+            return requestBody
+        } catch {
+            print("json error: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    func convertDataToDictionary(with data:Data?)->NSDictionary{
+        do { // try to parse JSON and deal with errors using do/catch block
+            let jsonDictionary: NSDictionary =
+                try JSONSerialization.jsonObject(with: data!,
+                                              options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+            
+            return jsonDictionary
+            
+        } catch {
+            
+            if let strData = String(data:data!, encoding:String.Encoding(rawValue: String.Encoding.utf8.rawValue)){
+                            print("printing JSON received as string: "+strData)
+            }else{
+                print("json error: \(error.localizedDescription)")
+            }
+            return NSDictionary() // just return empty
+        }
+    }
+    
+}
---
+++ ScanningApp/Controllers/SpeechRecognizer.swift
@@ -0,0 +1,138 @@
+//
+//  SpeechRecognizer.swift
+//  ScanningApp
+//
+//  Created by Amor on 2021/12/3.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import Speech
+import UIKit
+import ARKit
+
+class SpeechRecognizer{
+    // MARK: Properties
+    // The speech recogniser used by the controller to record the user's speech.
+    private let speechRecogniser = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+        
+    // The current speech recognition request. Created when the user wants to begin speech recognition
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+        
+    // The current speech recognition task. Created when the user wants to begin speech recognition.
+    private var recognitionTask: SFSpeechRecognitionTask?
+        
+    // The audio engine used to record input from the microphone.
+    private let audioEngine = AVAudioEngine()
+    
+    var spokenText = "" {
+        willSet(newValue) {
+            print("spokenText: \(spokenText)")
+            
+        }
+    }
+    
+    private var sceneView: ARSCNView
+    
+    init(sceneView: ARSCNView){
+        self.sceneView = sceneView
+    }
+    
+    // MARK: SFAudioTranscription
+    func startRecording() {
+        // setup recongizer
+        guard speechRecogniser.isAvailable else {
+            // Speech recognition is unavailable, so do not attempt to start.
+            return
+        }
+        
+        // make sure we have permission
+        guard SFSpeechRecognizer.authorizationStatus() == .authorized else {
+            SFSpeechRecognizer.requestAuthorization({ (status) in
+                // Handle the user's decision
+                print(status)
+            })
+            return
+        }
+        
+        
+        // setup audio
+        let audioSession = AVAudioSession.sharedInstance()
+        do{
+            try audioSession.setCategory(AVAudioSession.Category.record)
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        }
+        catch {
+            fatalError("Audio engine could not be setup")
+            
+        }
+
+        if recognitionRequest == nil {
+            // setup reusable request (if not already)
+            recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+            
+            // perform on device, if possible
+            // NOTE: this will usually limit the voice analytics results
+            if speechRecogniser.supportsOnDeviceRecognition {
+                print("Using on device recognition, voice analytics may be limited.")
+                recognitionRequest?.requiresOnDeviceRecognition = true
+            }else{
+                print("Using server for recognition.")
+            }
+        }
+        
+
+        let inputNode = audioEngine.inputNode
+        guard let recognitionRequest = recognitionRequest else {
+            // Handle error
+            return
+        }
+        
+        recognitionTask = speechRecogniser.recognitionTask(with: recognitionRequest) { [unowned self] result, error in
+            if let result = result {
+                spokenText = result.bestTranscription.formattedString
+            }
+            
+            if result?.isFinal ?? (error != nil) {
+                // this will remove the listening tap
+                // so that the transcription stops
+                inputNode.removeTap(onBus: 0)
+            }
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            recognitionRequest.append(buffer)
+        }
+
+        audioEngine.prepare()
+        do{
+            try audioEngine.start()
+        }
+        catch {
+            fatalError("Audio engine could not start")
+        }
+    }
+    
+    func stopRecording() {
+        if audioEngine.isRunning{
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+        }
+    }
+    
+    func didLongPress(_ gesture: UILongPressGestureRecognizer) {
+        print("startRecording didLongPress")
+        startRecording()
+    }
+    
+    func didTap(_ gesture: UITapGestureRecognizer) {
+        print("stopRecording didTap")
+        stopRecording()
+    }
+    
+    
+    
+}
